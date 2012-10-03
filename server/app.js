d3faa81af1369f25446c6b9254cd96344f332da9
harry <czy88840616@gmail.com>
2012-10-03 10:59:51 +0800

    web ok

---
+++ server/app.js
@@ -0,0 +1,61 @@
+/**
+ * @fileoverview
+ * @author Harry Chen <zhangting@taobao.com>
+ *
+ */
+
+var express = require('express')
+    , routes = require('./routes')
+    , user = require('./routes/user')
+    , http = require('http')
+    , path = require('path')
+    , render = require('../lib/render');
+
+function init(cfg) {
+
+    var app = express(),
+        nconf = cfg.nconf;
+
+    app.configure(function () {
+        app.set('port', cfg.port || 3000);
+        app.set('views', __dirname + '/views');
+        app.set('view engine', 'jade');
+        app.use(express.favicon());
+        app.use(express.logger('dev'));
+        app.use(express.bodyParser());
+        app.use(express.methodOverride());
+        app.use(app.router);
+        app.use(express.static(path.join(__dirname, 'public')));
+    });
+
+    app.configure('development', function () {
+        app.use(express.errorHandler({ dumpExceptions:true, showStack:true }));
+    });
+
+    app.configure('production', function(){
+        app.use(express.errorHandler());
+    });
+
+    app.get('/', routes.index);
+    app.get('/users', user.list);
+
+    var apps = nconf.get('apps');
+    for (var key in apps) {
+        app.all('/' + key +'/*.*htm*', function(req, res){
+            var template = render.parse({
+                app: key,
+                path: req.params[0],
+                parameters: require('url').parse(req.url).query
+            }, apps[key]);
+
+            template.render(req, res);
+        });
+    }
+
+    http.createServer(app).listen(app.get('port'), function () {
+        console.log("Express server listening on port " + app.get('port'));
+    });
+
+}
+
+module.exports.init = init;
