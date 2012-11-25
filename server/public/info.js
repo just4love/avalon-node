96bab7368ba8e87e1f1bfbd21e6a0b2e54a11a26
czy88840616 <czy88840616@gmail.com>
2012-11-25 21:41:03 +0800

    换了个插件

--- server/public/info.js
+++ server/public/info.js
@@ -9,4 +9,7 @@ $(function () {
         showLine: true,
         checkable: true
     }, $.parseJSON(data));
+
+    SyntaxHighlighter.config.clipboardSwf = '/syntaxhighlighter/scripts/clipboard.swf';
+    SyntaxHighlighter.all();
 });
