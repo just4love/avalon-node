5c24165248e55125162cb0cce93572fa1c687991
czy88840616 <czy88840616@gmail.com>
2012-12-10 00:37:34 +0800

    add testcase

---
+++ test/velocity/test.js
@@ -0,0 +1,12 @@
+/**
+ * @fileoverview
+ * @author Harry <czy88840616@gmail.com>
+ *
+ */
+var Jsonify = require('velocity.js/helper/Jsonify');
+
+describe('test velocity', function() {
+    it('test structure', function() {
+        console.log(Jsonify);
+    });
+});
