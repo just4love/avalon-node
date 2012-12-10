32a600efe4acdec2970e788b19b5c34a3e604345
czy88840616 <czy88840616@gmail.com>
2012-12-11 00:56:58 +0800

    add test

--- test/velocity/test.js
+++ test/velocity/test.js
@@ -3,10 +3,17 @@
  * @author Harry <czy88840616@gmail.com>
  *
  */
-var velocity = require('velocity.js');
+var velocity = require('velocity.js'),
+    helper = velocity.Helper,
+    Parser = velocity.Parser,
+    fs = require('fs');
 
 describe('test velocity', function() {
     it('test structure', function() {
-        console.log(Jsonify);
+        var Structure = new helper.Structure();
+        var vm = fs.readFileSync('../myCartBeta.vm').toString();
+        var asts = Parser.parse(vm);
+        var struct = new Structure(asts);
+        console.log(struct.context);
     });
 });
