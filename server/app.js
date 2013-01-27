39e78407fbb474c7950d262c36a31a74a934f5e1
harry <czy88840616@gmail.com>
2013-01-28 00:56:55 +0800

    fix parse vmcomon

--- server/app.js
+++ server/app.js
@@ -169,13 +169,13 @@ var processUrl = function(uri, domain,  callback){
 
     if(!isMatch) {
         if(!proxyDomain[domain]) {
-            console.log('请配置一条域名转换以避免死循环, domain='+domain);
+            console.log('[' + 'WARN'.yellow + '] 请配置一条域名转换以避免死循环, domain=%s',domain.cyan);
         }
         //没匹配到的，必须要过滤域名为ip
         uri = proxyDomain[domain] + uri;
     } else if(!util.isLocalFile(uri)) {
         if(!proxyDomain[domain]) {
-            console.log('请配置一条域名转换以避免死循环, domain='+domain);
+            console.log('[' + 'WARN'.yellow + '] 请配置一条域名转换以避免死循环, domain=%s',domain.cyan);
         }
         uri = proxyDomain[domain] + uri;
     }
